048c5c87ebb7661454af0d873828bc21549ef4ca
Evan Turner <evturn@gmail.com>
2015-02-26 18:05:30 -0500

    services adding voting functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,40 +21,30 @@ var app = angular
     'ngTouch',
     'firebase'
   ]);
-  app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/posts/:postId', {
-        templateUrl: 'views/showpost.html',
-        controller: 'PostViewCtrl'
-      })
-      .when('/register', {
-        templateUrl: 'views/register.html',
-        controller: 'AuthCtrl',
-        resolve: {
-          user: function(Auth) {
-            return Auth.resolveUser();
-          }
-        }
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'AuthCtrl',
-        resolve: {
-          user: function(Auth) {
-            return Auth.resolveUser();
-          }
-        }
-      })
-      .when('/users/:userId', {
-        templateUrl: 'views/profile.html',
-        controller: 'ProfileCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/users/:username', {
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
   .constant('FIREBASE_URL', 'https://vlox.firebaseio.com/');
