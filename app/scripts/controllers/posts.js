007ed4c47b721317328e32409946ac969d63c064
Evan Turner <evturn@gmail.com>
2015-02-26 02:22:15 -0500

    markup for posts page redone

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,11 +1,10 @@
 'use strict';
 
-app.controller('PostsCtrl', 
-	function($scope, $location, Post) {
-		$scope.posts = Post.all;
-		$scope.post = {url: 'http://', title: ''};
+app.controller('PostsCtrl', function($scope, $location, Post) {
+	$scope.posts = Post.all;
+	$scope.post = {url: 'http://', title: ''};
 
-		$scope.deletePost = function(post) {
-			Post.delete(post);
-		};
+	$scope.deletePost = function(post) {
+		Post.delete(post);
+	};
 });
