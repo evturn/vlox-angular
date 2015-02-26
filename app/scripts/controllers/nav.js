a9139dfd8777f52b9c199dd8567862bac38ca51f
Evan Turner <evturn@gmail.com>
2015-02-26 18:14:37 -0500

    nav ctrl refactored

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,19 +1,16 @@
 'use strict';
 
 app.controller('NavCtrl', function($scope, $location, Post, Auth) {
-	$scope.post = {url: 'http://', title: ''};
+  $scope.post = { url: 'http://', title: ''};
 
-		$scope.submitPost = function() {
-			$scope.post.creator = $scope.user.profile.username;
-			$scope.post.creatorUID = $scope.user.uid;
+  $scope.submitPost = function() {
+    Post.create($scope.post).then(function(postId) {
+      $scope.post = { url: 'http://', title: ''};
+      $location.path('/posts/' + postId);
+    });
+  };
 
-			Post.create($scope.post).then(function(ref) {
-				$location.path('/posts/' + ref.name());
-				$scope.post = {url: 'http://', title: ''};
-			});
-		};
-
-		$scope.signedIn = Auth.signedIn;
-		$scope.logout = Auth.logout;
-
+  $scope.logout = function() {
+    Auth.logout();
+  };
 });
