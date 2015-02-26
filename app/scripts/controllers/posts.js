61697ffa3ca0b65c1fdbc6a0b1359ad0e43e3ef3
Evan Turner <evturn@gmail.com>
2015-02-26 02:09:17 -0500

    NavCtrl added

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,12 +5,6 @@ app.controller('PostsCtrl',
 		$scope.posts = Post.all;
 		$scope.post = {url: 'http://', title: ''};
 
-		$scope.submitPost = function() {
-			Post.create($scope.post).then(function(ref) {
-				$location.path('/posts/' + ref.name());
-			});
-		};
-
 		$scope.deletePost = function(post) {
 			Post.delete(post);
 		};
