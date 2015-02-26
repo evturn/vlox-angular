b7b23314da4d83835f34285fa57343717055016f
Evan Turner <evturn@gmail.com>
2015-02-25 21:19:01 -0500

    function for deletePost fixed

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,7 +7,7 @@ app.controller('PostsCtrl', function($scope) {
 		$scope.post = {url: 'http://', title: ''};
 	};
 
-	$scope.deletePost = function($index) {
+	$scope.deletePost = function(index) {
 		$scope.posts.splice(index, 1);
 	};
 
--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,3 +1,5 @@
+'use strict';
+
 app.factory('Post', function($resource) {
 	return $resource('https://https://vlox.firebaseio.com/posts/:id.json');
 });
